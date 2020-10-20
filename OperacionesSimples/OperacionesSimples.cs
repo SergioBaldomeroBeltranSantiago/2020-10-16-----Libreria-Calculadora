616732dbc33bf3ac82913546034d27cbb0d46614
sergiobaldomerobeltran@gmail.com <sergiobaldomerobeltran@gmail.com>
2020-10-19 17:47:28 -0700

    Agregar archivos de proyecto.

---
+++ OperacionesSimples/OperacionesSimples.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Calculadora
+{
+    public class OperacionesSimples
+    {
+        public double Suma(double entuno, double entdos) {
+            return entuno + entdos;
+        }
+
+        public double Resta(double entuno, double entdos) {
+            return entuno - entdos;
+        }
+
+        public double Mult(double entuno, double entdos) {
+            return entuno * entdos;
+        }
+
+        public double Div(double entuno, double entdos) {
+            double salida=0;
+            try
+            {
+                salida = entuno / entdos;
+                return salida;
+            }
+            catch (DivideByZeroException) {
+                Console.WriteLine("\nDivision invalida de {0} entre cero",entuno);
+                return 0;
+            }
+        }
+    }
+}
